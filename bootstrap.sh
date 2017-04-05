6b36490c908eca6796aed9d99b731c42ba4b93a5
Christian Pelster <cpelster@thoughtworks.com>
2017-04-05 22:32:40 +0200

    support for linking outside of user home

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,11 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACKUP_DIR="${DIR}/backup"
 TIMESTAMP=$(date +%Y%m%d%H%M)
+DRY_RUN=${DRY_RUN:-true}
+
+if [ "${DRY_RUN}" = true ] ; then
+  echo "Performing a dry run..."
+fi
 
 if [ ! -d ${BACKUP_DIR} ] ; then
   mkdir ${BACKUP_DIR} || true
@@ -49,21 +54,94 @@ fi
 )
 
 function do_bootstrap() {
-  cd "$DIR/home"
-  FILES=$(find . -maxdepth 1 -mindepth 1)
-  for FILE in $FILES; do
-    
-    if [ -L ~/${FILE} ] ; then
-      echo "${FILE} is already linked"
-    else
-      if [ -f ~/${FILE} ] || [ -d ~/${FILE} ] ; then
-        echo "${FILE} exists and is not a link, creating a backup"
-        mkdir -p ${BACKUP_DIR}/${TIMESTAMP}
-        mv ~/${FILE} ${BACKUP_DIR}/${TIMESTAMP}
-      fi
+  {
+    cd "${DIR}/home"
+    link_files "" "$(realpath ~)"
+  }
+  { 
+    cd "${DIR}/etc"
+    link_files "" "/etc"
+  }
+}
 
-      ln -s ${DIR}/home/${FILE} ~
+function link_file() {
+  local link_name=$1
+  local link_target=$2
+  local level=$3
+
+  if [ -L ${link_name} ] ; then
+    echo "$(indent $level)${link_name} is already linked"
+    return 
+  fi
+
+  if [ -f ${link_name} ] || [ -d ${link_name} ] ; then
+    echo "$(indent $level)${link_name} exists and is not a link, creating a backup"
+    local backup_target="${BACKUP_DIR}/${TIMESTAMP}"
+    local src="${link_name}"
+
+    if [ "${DRY_RUN}" = true ] ; then
+      echo "$(indent $level)[dry run] Would backup ${link_name} to ${backup_target}"
+    else
+      mkdir -p ${backup_target}
+      if [ -w $(dirname ${link_name}) ] ; then
+        echo "--"
+        mv ${link_name} ${backup_target}
+      else
+        echo "ddd"
+        sudo mv ${link_name} ${backup_target}
+      fi
     fi
+
+  fi
+
+  if [ "${DRY_RUN}" = true ] ; then
+    echo -n "$(indent $level)[dry run] Would link ${link_name} to ${link_target}"
+    if [ -w $(dirname ${link_name}) ] ; then
+      echo ""
+    else
+      echo " using sudo"
+    fi
+  else
+    if [ -w $(dirname ${link_name}) ] ; then
+      ln -s ${link_target} ${link_name}
+    else
+      sudo ln -s ${link_target} ${link_name}
+    fi
+  fi
+}
+
+function indent() {
+  head -c $(($1*4)) < /dev/zero | tr '\0' ' '
+}
+
+function link_files() {
+  local current_dir=$1
+  local link_root=$2
+  local level=$(echo "${current_dir}" | grep -o / | wc -l)
+
+  echo "$(indent $level)looking for candidates in $(pwd)/${current_dir} to link to ${link_root}"
+ 
+  level=$(($level+1))
+  FILES=$(find ./${current_dir} -maxdepth 1 -mindepth 1 -not -name .do_not_link)
+  for FILE in $FILES; do
+    FILE=${FILE#./} 
+    local link_name="${link_root}/${FILE}"
+
+    echo -n "$(indent $level)candidate ${FILE}" 
+    if [ -d ${FILE} ] ; then
+      echo -n " is a directory"
+      if [ -f ${FILE}/.do_not_link ] ; then
+        echo " that should not be linked" 
+        link_files ${FILE} ${link_root} 
+      else
+        echo " that should be linked"
+        link_file "${link_name}" "$(pwd)/${FILE}" $(($level+1))
+      fi
+    else
+      echo " is a file " 
+      link_file "${link_name}" "$(pwd)/${FILE}" $(($level+1))
+    fi
+
     
   done
 }
